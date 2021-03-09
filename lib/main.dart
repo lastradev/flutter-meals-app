2131d40a6373bf16c94f4189c76fb488fd6eb636
Oscar Lastra <oscar.glastra@gmail.com>
2021-03-09 09:40:40 -0600

    Add bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -34,16 +34,6 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
-      // onGenerateRoute: (settings) {
-      //   if(settings.name == '/meal-detail'){
-      //     return ...;
-      //   }else if(settings.name == '/something-else'){
-      //     return ...;
-      //   }
-      //   return MaterialPageRoute(
-      //     builder: (ctx) => CategoriesScreen(),
-      //   );
-      // },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
       },
