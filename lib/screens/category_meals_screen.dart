62aea07816c1b5bda3c84ee4115ee1436349d324
Oscar Lastra <oscar.glastra@gmail.com>
2021-03-08 22:50:54 -0600

    Add top bar tabs

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals_app/widgets/meal_item.dart';
+import '../widgets/meal_item.dart';
 
 import '../dummy_data.dart';
 
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_meals_app/screens/meal_detail_screen.dart';
+import '../screens/meal_detail_screen.dart';
 
 import '../models/meal.dart';
 
